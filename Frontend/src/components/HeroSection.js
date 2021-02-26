3afabc290e94d38f2f030f9f514102d71d480276
pavit1809 <pavitragoyal@hotmail.com>
2021-02-26 21:25:51 +0530

    home-bgi

--- Frontend/src/components/HeroSection.js
+++ Frontend/src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img src='/images/home.jpg' className="home-img"/>
+      <img src='/images/homech1.jpg' className="home-img"/>
       <h1>HealthCare Documenting Made Easy</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
